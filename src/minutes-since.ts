4130377716825c2f50271a7fbd0e3d2042f436b0
Brandon Yanofsky <byanofsky@me.com>
2022-03-31 22:22:26 -0700

    Add minutes since function

---
+++ src/minutes-since.ts
@@ -0,0 +1,39 @@
+import { parser } from "./parser";
+
+export const minutesSince = (time: string): number => {
+  const tokens = parser.parse(time);
+  const hoursToken = tokens[0];
+
+  if (hoursToken == null) {
+    throw new Error(`InvalidTime: Hour must be defined.`);
+  }
+
+  const hours = Number(hoursToken);
+  if (hours < 0 || hours > 24) {
+    throw new Error(`InvalidTime: Hour out of range.`);
+  }
+
+  const minutesToken = tokens[1]?.[1];
+  const minutes = minutesToken != null ? Number(minutesToken) : 0;
+
+  if (minutes != null && (minutesToken < 0 || minutesToken > 59)) {
+    throw new Error(`InvalidTime: Minute out of range.`);
+  }
+
+  const periodToken = tokens[2];
+
+  let minutesPastMidnight = 0;
+
+  if (periodToken) {
+    minutesPastMidnight = (hours % 12) * 60;
+    if (periodToken === "pm") {
+      minutesPastMidnight += 12 * 60;
+    }
+  } else {
+    minutesPastMidnight = (hours % 24) * 60;
+  }
+
+  minutesPastMidnight += minutes;
+
+  return minutesPastMidnight;
+};
---
+++ tst/minutes-since.test.ts
@@ -0,0 +1,43 @@
+import { minutesSince } from "../src/minutes-since";
+
+interface TestCase {
+  input: string;
+  expected: number;
+}
+
+describe("hours", () => {
+  const testCases: TestCase[] = [
+    { input: "00:00", expected: 0 },
+    { input: "24:00", expected: 0 },
+    { input: "01:00", expected: 60 },
+    { input: "12:00", expected: 720 },
+    { input: "13:00", expected: 780 },
+    { input: "23:00", expected: 1380 },
+    { input: "12:00am", expected: 0 },
+    { input: "01:00am", expected: 60 },
+    { input: "12:00pm", expected: 720 },
+    { input: "1:00pm", expected: 780 },
+    { input: "11:00pm", expected: 1380 },
+  ];
+
+  testCases.forEach(({ input, expected }) => {
+    it(`with input ${input}`, () => {
+      expect(minutesSince(input)).toEqual(expected);
+    });
+  });
+});
+
+describe("minutes", () => {
+  const testCases: TestCase[] = [
+    { input: "03:01", expected: 181 },
+    { input: "03:01am", expected: 181 },
+    { input: "03:35", expected: 215 },
+    { input: "03:35am", expected: 215 },
+  ];
+
+  testCases.forEach(({ input, expected }) => {
+    it(`with input ${input}`, () => {
+      expect(minutesSince(input)).toEqual(expected);
+    });
+  });
+});
